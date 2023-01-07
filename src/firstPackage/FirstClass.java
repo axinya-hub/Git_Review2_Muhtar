6245fe1cc1308979f944e8e635792fae5c24743c
axinya <114306624+axinya-hub@users.noreply.github.com>
2023-01-07 12:33:13 -0700

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        // Some changes are made
+          System.out.println("First Class");
+       
+        
+        
     }
 
 
